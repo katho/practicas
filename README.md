f3817381f31bad9c37a5ac2a636d07c5a044bf48
Catarino <katarino240sx@gmail.com>
2013-08-21 21:04:32 -0500

    Creación del repo vacío

---
+++ README.md
@@ -0,0 +1,5 @@
+"# Este proyecto es para realizar prácticas de todo tipo sin arriesgar algún repo
+quu ya haya sido creado. Se podrán agregar y retirar controladores, bibliotecas
+de clases y demás recursos para realizar alguna tarea.
+Es necesario realizar una breve pero concreta descripción en el commit para
+saber cual fue el cambio." 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practicas::Application.config.secret_key_base = 'fa72b2ad903e2c981e45b69b95cb10c5ec6e5e2ec2c89741f10a11666e3de004010bce721181a3fd0e92ceb48355f89449a749dba9a60f39c930815fa45af687'
